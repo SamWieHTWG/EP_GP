f2fe03753b5de514edc6bc8e7864eb6c8b8ee42d
Samuel Wiertz <wiertzsamuel@gmail.com>
2019-01-11 16:54:55 +0100

    added GP.py

--- GP_Python/GP_Python.py
+++ GP_Python/GP_Python.py
@@ -1,4 +1,16 @@
-import numpy as np
 
-## Read Training-Data
+from functions.read_Train_Data import read_Train_Data
+
+
+
+# author: Samuel Wiertz, wiertzsamuel@gmail.com
+# date: 11.01.2019
+
+
+## read Train Data
+train_data = read_Train_Data()
+
+
+
+
 
---
+++ GP_Python/GP_module/GP.py
@@ -0,0 +1,9 @@
+
+class GP:
+
+    sig_f = 1
+    sig_n = 0.1
+    ell = 1
+
+
+    def
