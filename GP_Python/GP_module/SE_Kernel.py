317032379907f9eb9e9a35399a1316a25aef6951
Samuel Wiertz <wiertzsamuel@gmail.com>
2019-01-12 19:20:38 +0100

    some additional kernel calculations

--- GP_Python/GP_module/SE_Kernel.py
+++ GP_Python/GP_module/SE_Kernel.py
@@ -1,24 +1,37 @@
 import numpy as np
+import math
 
-def SE_Kernel(X1, X2, s_n, ell, s_f):
+def SE_Kernel(x1, x2, ell, s_f):
 
-    y = 0
-    l1 = np.size(X1,1)
-    l2 = np.size(X2,1)
+
+    s1 = x1.shape
+    s2 = x2.shape
+
+    l1 = s1[0]
+    l2 = s2[0]
 
     index_1 = np.arange(l1)
     index_2 = np.arange(l2)
 
+    k = np.zeros((l1, l2))
+
     # use: np.vectorize
     for ii in index_1:
         for jj in index_2:
+            k[ii, jj] = 2 * s_f * math.exp( -1/(2*ell^2) \
+                np.linalg.norm()
 
-            y = y + x1 + x2
 
-    return y
 
-x1 = np.array([2, 1, 4])
-x2 = np.array([3, 2, 1])
-print( SE_Kernel(x1,x2,0.1,1,1) )
+
+
+    return k
+
+x1 = np.matrix('1 2; 3 4; 2 3')
+
+x2 = np.matrix('1 2; 3 4')
+
+
+print( SE_Kernel(x1,x2,1,1) )
 
 
