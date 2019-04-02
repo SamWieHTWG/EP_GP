3334dfbc5521336ad3b61c48f8579eecba6880c6
Samuel Wiertz <wiertzsamuel@gmail.com>
2019-04-02 16:16:26 +0200

    train data 3640 added, new GP created

--- GP_Python/EP_project_samuel_wiertz.py
+++ GP_Python/EP_project_samuel_wiertz.py
@@ -17,14 +17,15 @@ y_train_p_gain = train_data['P']
 y_train_i_gain = train_data['I']
 
 
-# get Parameters for Train Data
+### create Random search Objects
+# Random Search for P-Gain gaussian process
 P_Parameter_Search = RandomSearch(lower_bound=HYPERPARAMETER_LOWER_BOUND,
                                   upper_bound=HYPERPARAMETER_UPPER_BOUND)
-
+# Random Search for I-Gain gaussian process
 I_Parameter_Search = RandomSearch(lower_bound=HYPERPARAMETER_LOWER_BOUND,
                                   upper_bound=HYPERPARAMETER_UPPER_BOUND)
 
-
+# do Hyper.-Opt. : true: optimize parameters to loaded training data, false: use previous parameters
 if DO_HYERPARAMETER_OPTIMIZATION:
     P_Parameter_Search.optimize_parameters(num_iterations=HYPERPARAMETER_OPTIMIZATION_ITERATIONS,
                                            optimization_data_x=x_train,
@@ -49,14 +50,15 @@ GP_P = GaussianProcess(x_train, y_train_p_gain, p_gain_optimal_parameters[0],
 GP_I = GaussianProcess(x_train, y_train_i_gain, i_gain_optimal_parameters[0],
                        i_gain_optimal_parameters[1], i_gain_optimal_parameters[2])
 
+# store created processes
+#store_gaussian_process(gaussian_process=GP_P, filename='3640_P')
+#store_gaussian_process(gaussian_process=GP_I, filename='3640_I')
 
-store_gaussian_process(gaussian_process=GP_P, filename='2693_P')
-store_gaussian_process(gaussian_process=GP_I, filename='2693_I')
-
+# load processes trained by highest amount of training data for rating
 GP_P2 = load_gaussian_process('2693_P')
 GP_I2 = load_gaussian_process('2693_I')
 
-rating = get_rating(GP_P2, GP_I2, 30, 1)
+rating = get_rating(GP_P2, GP_I2, 1000, 1)
 print('\nabsolute rating = ')
 print(rating)
 
